31ff867587f24cf1227e88ac574230627bcfa335
Кирилл Гордиенко <Kirill.55849@outlook.com>
2023-01-24 02:05:42 +0200

    немного обработал отображение View на сайте

--- WebApp3/Domains/Group.cs
+++ WebApp3/Domains/Group.cs
@@ -10,15 +10,15 @@ namespace WebApp3.Domains
 
         [ForeignKey("Curator")]
         public int? CuratorId { get; set; }
-        public Teacher Curator { get; set; }
+        public Teacher? Curator { get; set; }
 
         public int? SpecialId { get; set; }
         [ForeignKey("SpecialId")]
-        public Special Special { get; set; }
+        public Special? Special { get; set; }
 
         public int? CourseId { get; set; }
         [ForeignKey("CourseId")]
-        public Course Course { get; set; }
+        public Course? Course { get; set; }
 
         public IList<Student> Students { get; set; } = new List<Student>();
     }
